68dd734b18fdf2384462ac95b99a8223146caaf4
shbz1980 <shahbazkhader@gmail.com>
2021-02-12 12:07:52 +0100

    FEATURE: both energy_control_module and param_exp have been developed for both stochastic param PPO and CEM. This was a mistake. The last state for this brach is being committed. A new brach for CEM will be started. These files are also likeliy to contain bugs. A known bug is the high values couputted by the gradinet of ICNN for different random seeds. For some random seeds it works ok.

--- policies/energy_control_modules.py
+++ policies/energy_control_modules.py
@@ -4,6 +4,8 @@ from garage.torch import NonLinearity
 import torch
 import torch.nn as nn
 
+# all netwrok settings are done directly here. todo
+
 class PositiveLinear(nn.Module):
     def __init__(self, in_features, out_features):
         super(PositiveLinear, self).__init__()
@@ -14,41 +16,133 @@ class PositiveLinear(nn.Module):
     def forward(self, input):
         return nn.functional.linear(input, self.log_weight.exp())
 
+class LinearList(nn.Module):
+    def __init__(self, input_dim, output_dim, col=True):
+        super(LinearList, self).__init__()
+        self.p_list = nn.ParameterList()
+        self.col = col
+        for i in range(input_dim if col else output_dim):
+            if col:
+                p = nn.Parameter(torch.Tensor(output_dim , 1))
+            else:
+                p = nn.Parameter(torch.Tensor(1, input_dim))
+            self.p_list.append(p)
+        self.bias = nn.Parameter(torch.Tensor(output_dim))
+
+
+    def init(self, w_init, b_init, w_init_const=None):
+        for p in self.p_list:
+            if w_init_const:
+                w_init(p, w_init_const)
+            else:
+                w_init(p)
+        b_init(self.bias)
+
+    def forward(self, input):
+        weights = [p for p in self.p_list]
+        if self.col:
+            weight = torch.cat(weights, dim=1)
+        else:
+            weight = torch.cat(weights, dim=0)
+        return nn.functional.linear(input, weight, self.bias)
+
+    def get_weight(self):
+        weights = [p for p in self.p_list]
+        if self.col:
+            weight = torch.cat(weights, dim=1)
+        else:
+            weight = torch.cat(weights, dim=0)
+        return weight
+
+    def get_bias(self):
+        return self.bias
+
+
+class PositiveLinearList(LinearList):
+
+    def __init__(self, input_dim, output_dim, col=True):
+        super(LinearList, self).__init__()
+        self.p_list = nn.ParameterList()
+        self.col = col
+        self.output_dim = output_dim
+        for i in range(input_dim if col else output_dim):
+            if col:
+                p = nn.Parameter(torch.Tensor(output_dim , 1))
+            else:
+                p = nn.Parameter(torch.Tensor(1, input_dim))
+            self.p_list.append(p)
+
+
+    def init(self, w_init, w_init_const=None):
+        for p in self.p_list:
+            if w_init_const:
+                w_init(p, w_init_const)
+            else:
+                w_init(p)
+
+    def forward(self, input):
+        weights = [p for p in self.p_list]
+        if self.col:
+            weight = torch.cat(weights, dim=1)
+        else:
+            weight = torch.cat(weights, dim=0)
+        return nn.functional.linear(input, weight.exp())
+
+    def get_weight(self):
+        weights = [p for p in self.p_list]
+        if self.col:
+            weight = torch.cat(weights, dim=1)
+        else:
+            weight = torch.cat(weights, dim=0)
+        return weight.exp()
+
+# log normal mean and std calculation
+
+
+desired_lognormal_mean = torch.tensor(0.1)         #todo
 
 class ICNN(nn.Module):
-    # check if all params are registered todo
+
     def __init__(self,
                  input_dim,
                  hidden_sizes,
                  w_init=nn.init.xavier_normal_,
                  b_init=nn.init.zeros_,
                  nonlinearity=torch.relu,
+                 init_std=0.1
                  ):
         super(ICNN, self).__init__()
 
+        # log normal stuff
+        self.desired_mean = desired_lognormal_mean
+        self.desired_std = init_std
+        self.log_mean = torch.log((self.desired_mean ** 2) / torch.sqrt((self.desired_mean ** 2 + self.desired_std ** 2)))
+        self.log_std = torch.sqrt(torch.log(1 + (self.desired_std ** 2) / (self.desired_mean ** 2)))
+
         self._y_layers = nn.ModuleList()
         for size in hidden_sizes:
-            linear_layer = nn.Linear(input_dim, size)
-            w_init(linear_layer.weight)
-            b_init(linear_layer.bias) # check if bias is there todo
+            linear_layer = LinearList(input_dim, size, col=True)
+            linear_layer.init(nn.init.xavier_uniform_, nn.init.zeros_)
             self._y_layers.append(linear_layer)
-        linear_layer = nn.Linear(input_dim, 1)
-        w_init(linear_layer.weight)
-        b_init(linear_layer.bias)
+        linear_layer = LinearList(input_dim, 1, col=False)
+        linear_layer.init(nn.init.xavier_uniform_, nn.init.zeros_)
         self._y_layers.append(linear_layer)
 
         self._z_layers = nn.ModuleList()
         prev_size = hidden_sizes[0]
         for size in hidden_sizes[1:]:
-            positive_linear_layer = PositiveLinear(prev_size, size)
-            w_init(positive_linear_layer.log_weight)
+            positive_linear_layer = PositiveLinearList(prev_size, size, col=True)
+            positive_linear_layer.init(nn.init.constant_, self.log_mean)          #todo
+            # positive_linear_layer.init(nn.init.xavier_uniform_)
             self._z_layers.append(positive_linear_layer)
             prev_size = size
-        positive_linear_layer = PositiveLinear(prev_size, 1)
-        w_init(positive_linear_layer.log_weight)
+        positive_linear_layer = PositiveLinearList(prev_size, 1, col=False)
+        positive_linear_layer.init(nn.init.constant_, self.log_mean)
+        # positive_linear_layer.init(nn.init.xavier_uniform_)
         self._z_layers.append(positive_linear_layer)
 
-        self.nonlinearity = NonLinearity(nonlinearity)
+        self.nonlinearity = NonLinearity(torch.relu)
+        print('icnn init')
 
     def forward(self, z):
 
@@ -61,11 +155,45 @@ class ICNN(nn.Module):
             z = self.nonlinearity(self._y_layers[i+1](z0) + self._z_layers[i](z))
         return z
 
+    def relu_grad(self, z):
+        z_ = z.clone()
+        # z_.squeeze_()
+        z_[(z_ > 0.0)] = 1.0
+        z_[(z_ <= 0.0)] = 0.0
+        return z_
+
+    def grad_x(self, z):
+
+        dz1_dx = self._y_layers[0].get_weight()
+        z0 = z.clone()
+        z = self._y_layers[0](z0)
+        dz1r_dz1 = self.relu_grad(z)
+        dz1r_dz1_ = dz1r_dz1.unsqueeze(2).repeat(1, 1, dz1_dx.shape[1])
+        z = self.nonlinearity(z)
+        dz1_dx_ = dz1_dx.expand_as(dz1r_dz1_)
+        dz1r_dx = dz1r_dz1_ * dz1_dx_
+
+        layer_num = len(self._z_layers)
+        for i in range(layer_num):
+            z = self._y_layers[i + 1](z0) + self._z_layers[i](z)
+            dz1r_dz = self.relu_grad(z)
+            z = self.nonlinearity(z)
+            dzz1_dx = torch.matmul(self._z_layers[i].get_weight(), dz1r_dx)
+            dzy1_dx = self._y_layers[i + 1].get_weight()
+            dzy1_dx_plus_dzz1_dx = dzy1_dx + dzz1_dx
+            dz1r_dz_ = dz1r_dz.unsqueeze(2).repeat(1, 1, dzy1_dx_plus_dzz1_dx.shape[2])
+            dz1r_dx = dz1r_dz_ * dzy1_dx_plus_dzz1_dx
+
+        return dz1r_dx.squeeze(1)
+
+
+
 class Damping(nn.Module):
     def __init__(self,
                  input_dim,  # input should be velocity
                  hidden_sizes,
                  w_init=nn.init.xavier_normal_,
+                 w_init_const=1.0,
                  b_init=nn.init.zeros_,
                  hidden_nonlinearity=torch.tanh,
                  full_mat=True,
@@ -78,55 +206,148 @@ class Damping(nn.Module):
         self.output_dim = self.offdiag_output_dim + self.diag_output_dim
         self.full_mat = full_mat
 
-        self._layers = nn.ModuleList()
-
+        # diag
+        self._diag_layers = nn.ModuleList()
         prev_size = input_dim
         for size in hidden_sizes:
             hidden_layers = nn.Sequential()
-            linear_layer = nn.Linear(prev_size, size)
-            w_init(linear_layer.weight)
-            b_init(linear_layer.bias)
+            linear_layer = LinearList(prev_size, size, col=True)
+            linear_layer.init(nn.init.xavier_uniform_, nn.init.zeros_)
             hidden_layers.add_module('linear', linear_layer)
             hidden_layers.add_module('non_linearity',
-                                         NonLinearity(hidden_nonlinearity))
-            self._layers.append(hidden_layers)
+                                         NonLinearity(torch.tanh))
+            self._diag_layers.append(hidden_layers)
             prev_size = size
 
-        if self.full_mat:
-            out_dim = self.output_dim
-        else:
-            out_dim = self.diag_output_dim
+        self._diag_output_layer = nn.ModuleList()
+        linear_layer = LinearList(prev_size, self.diag_output_dim, col=False)
+        # linear_layer.init(nn.init.constant_, nn.init.zeros_, log_const_init)      # todo
+        linear_layer.init(nn.init.xavier_uniform_, nn.init.zeros_)
+        self._diag_output_layer.append(linear_layer)
 
-        self._output_layer = nn.ModuleList()
-        linear_layer = nn.Linear(prev_size, out_dim)
-        w_init(linear_layer.weight)
-        b_init(linear_layer.bias)
-        self._output_layer.append(linear_layer)
+        # offdiag
+        if self.full_mat:
+            self._offdiag_layers = nn.ModuleList()
+            prev_size = input_dim
+            for size in hidden_sizes:
+                hidden_layers = nn.Sequential()
+                linear_layer = LinearList(prev_size, size, col=True)
+                linear_layer.init(nn.init.xavier_uniform_, nn.init.zeros_)
+                hidden_layers.add_module('linear', linear_layer)
+                hidden_layers.add_module('non_linearity',
+                                         NonLinearity(torch.tanh))
+                self._offdiag_layers.append(hidden_layers)
+                prev_size = size
+
+            self._offdiag_output_layer = nn.ModuleList()
+            linear_layer = LinearList(prev_size, self.offdiag_output_dim, col=False)
+            linear_layer.init(nn.init.xavier_uniform_, nn.init.zeros_)
+            self._offdiag_output_layer.append(linear_layer)
+        print('damper init')
 
     def forward(self, input):
+        # todo this method is not ready for batch input data
+        # x = input.view(1,-1)
+        x = input
+        z = input
+        x0 = x.clone()
 
-        x = input.view(1,-1)
-        for layer in self._layers:
+        for layer in self._diag_layers:
             x = layer(x)
+        x = self._diag_output_layer[0](x)
 
-        x = self._output_layer[0](x)
-        n = self.diag_output_dim
-        diag_idx = np.diag_indices(n)
-        D = torch.zeros(x.shape[0], n, n)
+        if not self.full_mat:
+            assert(x.shape == x0.shape)
+            return torch.exp(x) * x0
+        else:
+            n = self.diag_output_dim
+            diag_idx = np.diag_indices(n)
+            off_diag_idx = np.tril_indices(n, k=-1)
+            D = torch.zeros(x0.shape[0], n)
 
-        for i in range(x.shape[0]):
-            L = torch.zeros(n, n)
-            if self.full_mat:
-                diag_elements = torch.exp(x[i, :self.diag_output_dim])
-                off_diag_elements = x[i, self.diag_output_dim:]
-                off_diag_idx = np.tril_indices(n, k=-1)
+            for layer in self._offdiag_layers:
+                z = layer(z)
+            z = self._offdiag_output_layer[0](z)
+
+            for i in range(x0.shape[0]):
+                L = torch.zeros(n, n)
+                diag_elements = x[i]
+                off_diag_elements = z[i]
                 L[off_diag_idx] = off_diag_elements
                 L[diag_idx] = diag_elements
-                D[i] = L@L.t()
-            else:
-                diag_elements = torch.exp(x[i])
-                D[i][diag_idx] = diag_elements
+                D_temp = L@L.t()
+                D[i] = D_temp @ x0[i]
+            return D
 
-        return D
+# class Damping(nn.Module):
+#     def __init__(self,
+#                  input_dim,  # input should be velocity
+#                  hidden_sizes,
+#                  w_init=nn.init.xavier_normal_,
+#                  w_init_const=1.0,
+#                  b_init=nn.init.zeros_,
+#                  hidden_nonlinearity=torch.tanh,
+#                  full_mat=True,
+#                  ):
+#         super(Damping, self).__init__()
+#
+#         N = input_dim
+#         self.offdiag_output_dim = N*(N-1)//2
+#         self.diag_output_dim = N
+#         self.output_dim = self.offdiag_output_dim + self.diag_output_dim
+#         self.full_mat = full_mat
+#
+#         self._layers = nn.ModuleList()
+#
+#         prev_size = input_dim
+#         for size in hidden_sizes:
+#             hidden_layers = nn.Sequential()
+#             linear_layer = LinearList(prev_size, size, col=True)
+#             linear_layer.init(w_init, b_init, w_init_const)
+#             hidden_layers.add_module('linear', linear_layer)
+#             hidden_layers.add_module('non_linearity',
+#                                          NonLinearity(hidden_nonlinearity))
+#             self._layers.append(hidden_layers)
+#             prev_size = size
+#
+#         if self.full_mat:
+#             out_dim = self.output_dim
+#         else:
+#             out_dim = self.diag_output_dim
+#
+#         self._output_layer = nn.ModuleList()
+#         linear_layer = LinearList(prev_size, out_dim, col=False)
+#         linear_layer.init(w_init, b_init, w_init_const)
+#         self._output_layer.append(linear_layer)
+#
+#     def forward(self, input):
+#         # todo this method is not ready for batch input data
+#         # x = input.view(1,-1)
+#         x = input
+#         x0 = x.clone()
+#         for layer in self._layers:
+#             x = layer(x)
+#
+#         x = self._output_layer[0](x)
+#         n = self.diag_output_dim
+#         diag_idx = np.diag_indices(n)
+#         D = torch.zeros(x0.shape[0], n)
+#
+#         for i in range(x.shape[0]):
+#             L = torch.zeros(n, n)
+#             if self.full_mat:
+#                 diag_elements = torch.exp(x[i, :self.diag_output_dim])
+#                 off_diag_elements = x[i, self.diag_output_dim:]
+#                 off_diag_idx = np.tril_indices(n, k=-1)
+#                 L[off_diag_idx] = off_diag_elements
+#                 L[diag_idx] = diag_elements
+#                 D_temp = L@L.t()
+#                 D[i] = D_temp @ x0[i]
+#             else:
+#                 diag_elements = torch.exp(x[i])
+#                 D[i] = torch.diag(diag_elements) @ x0[i]
+#
+#         # D = torch.exp(x) * x0
+#         return D
 
 
--- utilities/param_exp.py
+++ utilities/param_exp.py
@@ -1,9 +1,10 @@
-
+import numpy as np
 import torch
 import random
 import copy
 from energybased_stable_rl.policies.energy_based_control_policy import GaussianEnergyBasedPolicy
 from energybased_stable_rl.policies.gaussian_ps_mlp_policy import GaussianPSMLPPolicy
+from energybased_stable_rl.policies.energy_control_modules import desired_lognormal_mean
 
 # def perturb_layers(pre_str, state_dict_in, state_dict_out, std, strs, layers):
 #     for i in range(layers):
@@ -14,26 +15,34 @@ from energybased_stable_rl.policies.gaussian_ps_mlp_policy import GaussianPSMLPP
 #                 state_dict_out[pre_str + str(i) + s].copy_(perturbed_param)
 
 
-def perturbTorchPolicy(policy, std, jac_batch_size=64):
+def perturbTorchPolicy(policy, jac_batch_size=64):
+
     state_dict_in = policy.get_param_values()
     state_dict_out = copy.deepcopy(state_dict_in)
 
     state_dict_keys = list(state_dict_in.keys())
 
+    std = policy._module.get_std().detach()
+    state_dict_out['_module._init_std'].copy_(std)  # todo is this the right place for this?
     if isinstance(policy, GaussianPSMLPPolicy):
-        key_str = '_mean_module'
+        key_strs = ['_mean_module']
+
+    if isinstance(policy, GaussianEnergyBasedPolicy):
+        key_strs = ['_icnn_module', '_damping_module', '_quad_pot_param']
+        # key_strs = ['_icnn_module']
 
     param_keys_list = []
     selected_keys_list = []
 
     for str in state_dict_keys:
-        if key_str in str:
-            param_keys_list.append(str)
+        for key_str in key_strs:
+            if key_str in str:
+                param_keys_list.append(str)
 
     rnd_idx = random.sample(list(range(0, len(param_keys_list))), k=len(param_keys_list))
 
     batch_size_count = 0
-    while batch_size_count < jac_batch_size:
+    while batch_size_count < jac_batch_size and len(rnd_idx)>0:
         param_key = param_keys_list[rnd_idx.pop()]
         param = state_dict_in[param_key]
         batch_size_count = batch_size_count + len(param.view(-1))
@@ -42,6 +51,7 @@ def perturbTorchPolicy(policy, std, jac_batch_size=64):
         state_dict_out[param_key].copy_(perturbed_param)
 
     return (state_dict_out, selected_keys_list)
+
 
 
 
@@ -75,10 +85,67 @@ def perturbTorchPolicy(policy, std, jac_batch_size=64):
     #
     # return state_dict_out
 
-def perturbTorchPolicyBatch(policy, std, batch_size=1, jac_batch_size=64):
+def perturbTorchPolicyBatch(policy, batch_size=1, jac_batch_size=64):
     policy_list = []
 
     for i in range(batch_size):
-        policy_list.append(perturbTorchPolicy(policy, std, jac_batch_size=jac_batch_size))
+        policy_list.append(perturbTorchPolicy(policy, jac_batch_size=jac_batch_size))
 
     return policy_list
+
+def sample_params(mean_state_dict, std_dict, epoch):
+    state_dict_in = mean_state_dict
+    state_dict_out = copy.deepcopy(state_dict_in)
+
+    state_dict_keys = list(state_dict_in.keys())
+
+    key_strs = ['_icnn_module', '_damping_module', '_quad_pot_param'] #todo
+    # key_strs = ['_mean_module']
+
+    for key in state_dict_keys:
+        for key_str in key_strs:
+            if key_str in key:
+                param = state_dict_in[key]
+                std = std_dict[key]
+                perturbed_param = torch.normal(param, std)
+                state_dict_out[key].copy_(perturbed_param)
+    return state_dict_out
+
+def cem_init_std(state_dict, init_std, std_dict):
+    state_dict_keys = list(state_dict.keys())
+    key_strs = ['_icnn_module', '_damping_module', '_quad_pot_param']
+    log_normal_strs = ['_z_layers', 'p_list']
+    # key_strs = ['_mean_module']
+
+
+    for key in state_dict_keys:
+        for key_str in key_strs:
+            if key_str in key:
+                param = state_dict[key]
+                if (log_normal_strs[0] in key)  and (log_normal_strs[1] in key):         # log normal todo bad implementation
+                    desired_mean = torch.ones_like(param)*desired_lognormal_mean
+                    # desired_std = torch.ones_like(param) * init_std   # todo
+                    desired_std = torch.ones_like(param) * 0.001
+                    log_std = torch.sqrt(torch.log(1 + (desired_std ** 2) / (desired_mean ** 2)))
+                    std_dict[key] = log_std
+                else:
+                    std_dict[key] = torch.ones_like(param) * init_std
+    print('cem init')
+
+
+
+def cem_stat_compute(best_params, curr_mean, curr_std):
+    state_dict_keys = list(curr_mean.keys())
+
+    for key in state_dict_keys:
+        if (('_icnn_module' in key) or ('_damping_module' in key) or ('_quad_pot_param' in key)):
+        # if ('_mean_module' in key):
+            param_list = [params[key].unsqueeze(-1) for params in best_params]
+            stack_dim = len(param_list[0].shape)-1
+            param_array = torch.cat(param_list, axis=stack_dim)
+            curr_mean[key] = torch.mean(param_array, axis=stack_dim)
+            curr_std[key] = torch.std(param_array, axis=stack_dim)
+
+
+
+
