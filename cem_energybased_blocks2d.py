bda352fbace78a07e4350d2dff2f04d8eeb7bb63
shbz1980 <shahbazkhader@gmail.com>
2021-02-14 13:08:16 +0100

    CHECKPOINT: Working block 2d: init policy, no icnn bias, relu weight for icnn. Works for random seeds 1,2,3.

--- cem_energybased_blocks2d.py
+++ cem_energybased_blocks2d.py
@@ -12,6 +12,7 @@ from garage.envs import GymEnv
 from garage.experiment.deterministic import set_seed
 from energybased_stable_rl.algos.cem import CEM
 from energybased_stable_rl.policies.energy_based_control_policy import EnergyBasedPolicy
+from energybased_stable_rl.policies.energy_based_init_policy import EnergyBasedInitPolicy
 from garage.np.baselines import LinearFeatureBaseline
 from garage.trainer import Trainer
 from energybased_stable_rl.envs.block2D import T
@@ -36,37 +37,52 @@ def cem_energybased_block2d(ctxt=None, seed=1):
     trainer = Trainer(ctxt)
 
     # log normal stuff
-    desired_lognormal_mean = torch.tensor(0.3)
+    desired_lognormal_mean = torch.tensor(.3)
     desired_mean = desired_lognormal_mean
-    desired_std = 0.03
+    desired_std = 0.001
     log_mean = torch.log((desired_mean ** 2) / torch.sqrt((desired_mean ** 2 + desired_std ** 2)))
     log_std = torch.sqrt(torch.log(1 + (desired_std ** 2) / (desired_mean ** 2)))
     init_log_std = log_std
 
-    init_std = 0.03
 
-    damp_min = torch.ones(2)*1e-2
+    init_std = 0.1
+    init_log_std = 1.0
 
+    coord_dim = env.spec.observation_space.flat_dim // 2
+
+    damp_min = torch.ones(coord_dim) * 1e-3
+
+    icnn_bias = False
+
+    # init_policy = None
+    init_policy = EnergyBasedInitPolicy(env.spec,
+                                        S_param=torch.ones(coord_dim) * 2.0,
+                                        D_param=torch.ones(coord_dim) * 1.0,
+                                        std=2.0
+                                        )
 
     policy = EnergyBasedPolicy(env.spec,
                                        icnn_hidden_sizes=(16,16),
                                        w_init_icnn_y=nn.init.xavier_uniform_,
                                        b_init_icnn_y=nn.init.zeros_,
-                                       w_init_icnn_z=nn.init.constant_,
-                                       w_init_icnn_z_param=log_mean,
+                                        w_init_icnn_y_param=None,  # pass None if...
+                                       # w_init_icnn_z=nn.init.constant_,
+                                        w_init_icnn_z=nn.init.xavier_uniform_,
+                                       w_init_icnn_z_param=None,        # pass None if...
+                                       icnn_bias = icnn_bias,
+                                       positive_type = 'relu',
                                        nonlinearity_icnn=torch.relu,
                                        damper_hidden_sizes=(8, 8),
                                        w_init_damper_offdiag=nn.init.xavier_uniform_,
                                        b_init_damper_offdiag=nn.init.zeros_,
                                        w_init_damper_diag=nn.init.xavier_uniform_,
-                                       w_init_damper_diag_param=log_mean,
                                        b_init_damper_diag=nn.init.zeros_,
                                        hidden_nonlinearity_damper=torch.tanh,
                                        full_mat_damper=True,
                                        damp_min = damp_min,
                                        init_quad_pot=1.0,
                                        min_quad_pot=1e-3,
-                                       max_quad_pot=1e1,
+                                       max_quad_pot=0.5e1,
                                        icnn_min_lr=1e-4,)
 
     baseline = LinearFeatureBaseline(env_spec=env.spec)
@@ -80,8 +96,11 @@ def cem_energybased_block2d(ctxt=None, seed=1):
                baseline=baseline,
                best_frac=0.2,
                action_lt=5.0,
-               n_samples=n_samples)
+               n_samples=n_samples,
+               init_policy=init_policy,     # pass None if policy init is not required
+               min_icnn=icnn_bias)
 
+    # n_workers should be 1
     trainer.setup(algo, env, n_workers=1, sampler_cls=LocalSampler, worker_class=DefaultWorker)
 
     trainer.train(n_epochs=50, batch_size=T, plot=True, store_episodes=True)
@@ -91,28 +110,50 @@ try:
 except Exception:
     traceback.print_exc()
 
-# cem_block2d_20(seed=1)
-# policy = GaussianMLPPolicy(env.spec,
-#                                    hidden_sizes=[16, 16],
-#                                    hidden_nonlinearity=tf.nn.tanh,
-#                                    output_nonlinearity=None,
-#                                    learn_std=False,
-#                                    init_std=1.)
-#
+# cem_energybased_block2d_14(seed=1)
+# no icnn z bias, no icnn min, relu weight for icnn, non init policy
+# init_std = 0.1
+# init_log_std = init_std
+# coord_dim = env.spec.observation_space.flat_dim // 2
+# damp_min = torch.ones(coord_dim) * 1e-2
+# init_policy = None
+# policy = EnergyBasedPolicy(env.spec,
+#                                    icnn_hidden_sizes=(16,16),
+#                                    w_init_icnn_y=nn.init.xavier_uniform_,
+#                                    b_init_icnn_y=nn.init.zeros_,
+#                                    # w_init_icnn_z=nn.init.constant_,
+#                                     w_init_icnn_z=nn.init.xavier_uniform_,
+#                                    w_init_icnn_z_param=None,        # pass None if...
+#                                    icnn_bias = False,
+#                                    positive_type = 'relu',
+#                                    nonlinearity_icnn=torch.relu,
+#                                    damper_hidden_sizes=(8, 8),
+#                                    w_init_damper_offdiag=nn.init.xavier_uniform_,
+#                                    b_init_damper_offdiag=nn.init.zeros_,
+#                                    w_init_damper_diag=nn.init.xavier_uniform_,
+#                                    b_init_damper_diag=nn.init.zeros_,
+#                                    hidden_nonlinearity_damper=torch.tanh,
+#                                    full_mat_damper=True,
+#                                    damp_min = damp_min,
+#                                    init_quad_pot=1.0,
+#                                    min_quad_pot=1e-3,
+#                                    max_quad_pot=0.5e1,
+#                                    icnn_min_lr=1e-4,)
 # baseline = LinearFeatureBaseline(env_spec=env.spec)
-#
 # n_samples = 15
-#
 # algo = CEM(env_spec=env.spec,
 #            policy=policy,
-#            init_std=3.,
+#            init_std=init_std,
+#            init_log_std = init_log_std,
 #            baseline=baseline,
 #            best_frac=0.2,
-#            n_samples=n_samples)
-#
-# trainer.setup(algo, env, n_workers=4)
-#
-# trainer.train(n_epochs=100, batch_size=T, plot=True, store_episodes=True)
+#            action_lt=5.0,
+#            n_samples=n_samples,
+#            init_policy=init_policy,     # pass None if policy init is not required
+#            min_icnn=False)
+# # n_workers should be 1
+# trainer.setup(algo, env, n_workers=1, sampler_cls=LocalSampler, worker_class=DefaultWorker)
+# trainer.train(n_epochs=50, batch_size=T, plot=True, store_episodes=True)
 # OFFSET = np.array([0.4, -0.6])
 # OFFSET_1 = np.array([0, 0])         # NFPPPO fixed init
 # GOAL = np.array([0, 0.5])+OFFSET
@@ -125,3 +166,8 @@ except Exception:
 # w = 1
 # TERMINAL_STATE_SCALE = 10
 # size="0.05 0.048 0.05"
+
+# cem_energybased_block2d_14(seed=1)
+# no icnn z bias, no icnn min, relu weight for icnn, non init policy
+# init_log_std = 1.0
+# damp_min = torch.ones(coord_dim) * 1e-3
